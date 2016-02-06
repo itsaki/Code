490b5dac289483cc7ef7ffbda49e0b2cac7633c0
Akrati <itsakrati@gmail.com>
2016-02-06 15:17:06 -0800

    swap program

---
+++ Java Baby programs/src/SwapTwoNumbersWithoutThirdNumber.java
@@ -0,0 +1,13 @@
+
+public class SwapTwoNumbersWithoutThirdNumber {
+public static void main(String[] args){
+	int x = 20;
+	int y = 30;
+	
+	System.out.println("x=" + x + ", y=" + y);
+	x = x+y;
+	y = x-y;
+	x = x-y;
+	System.out.println("x=" + x + ", y=" + y);
+	}
+}
